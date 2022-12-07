59878af999b1079c30bb176f5ca955e45f4762e2
Tingzhao Wang <wang.tingzh@northeastern.edu>
2022-12-06 17:13:02 -0800

    initialize project

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  return <Router></Router>;
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
