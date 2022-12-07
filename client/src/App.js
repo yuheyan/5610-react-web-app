08e96f41261bd620956335d2f04ce0fadbde2e07
Tingzhao Wang <wang.tingzh@northeastern.edu>
2022-12-06 16:52:14 -0800

    initialize frontend client

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+
+function App() {
+  return <Router></Router>;
+}
+
+export default App;
