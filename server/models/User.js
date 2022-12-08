a15c44e768b4475db1e0e96027a5658598fe9abb
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-12-08 13:22:15 -0800

    styling profile and edit-profile, trying to add more user properties

--- server/models/User.js
+++ server/models/User.js
@@ -68,6 +68,10 @@ const UserSchema = new mongoose.Schema(
       enum: [1, 2, 3],
     },
     //add new fields here
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Prefer not to respond"]
+    },
   },
   { timestamps: true }
 );
