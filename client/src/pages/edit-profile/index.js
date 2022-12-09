ef5510eab243b765fd613ee1942bc310be80f1ac
Orangemoon8 <62192912+Orangemoon8@users.noreply.github.com>
2022-12-09 12:32:20 -0800

    fix form after styling

--- client/src/pages/edit-profile/index.js
+++ client/src/pages/edit-profile/index.js
@@ -33,8 +33,8 @@ export default function EditProfile() {
         userId:user._id,
         username:usernameInput.current.value,
         email:emailInput.current.value,
-        passwordInput:passwordInput.current.value,
-        genderInput:genderInput.current.value,
+        // passwordInput:passwordInput.current.value,
+        // genderInput:genderInput.current.value,
       });
   }
 
@@ -135,192 +135,194 @@ export default function EditProfile() {
                 </div>
               </div>
             </div>
+
             <div className="col-xl-8 order-xl-1">
-              <div className="card bg-secondary shadow">
-                <div className="card-header bg-white border-0">
-                  <div className="row align-items-center">
-                    <div className="col-8">
-                      <h3 className="mb-0">My account</h3>
+                <form onSubmit={handleSubmit}>
+                  <div className="card bg-secondary shadow">
+                    <div className="card-header bg-white border-0">
+                      <div className="row align-items-center">
+                        <div className="col-8">
+                          <h3 className="mb-0">My account</h3>
+                        </div>
+                        
+                      </div>
                     </div>
-                    
+                    <div className="card-body">
+                      <form>
+                        <h6 className="heading-small text-muted mb-4">
+                          User information
+                        </h6>
+                        <div className="pl-lg-4">
+                          <div className="row">
+                            <div className="col-lg-6">
+                              <div className="form-group focused">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-username"
+                                >
+                                  Username
+                                </label>
+                                <input
+                                  type="text"
+                                  id="input-username"
+                                  className="form-control form-control-alternative"
+                                  placeholder="Username"
+                                  defaultValue={user.username}
+                                  ref={usernameInput}
+                                />
+                              </div>
+                            </div>
+                            <div className="col-lg-6">
+                              <div className="form-group">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-email"
+                                >
+                                  Email address
+                                </label>
+                                <input
+                                  type="email"
+                                  id="input-email"
+                                  className="form-control form-control-alternative"
+                                  defaultValue={user.email}
+                                  //ref={emailInput}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-lg-6">
+                              <div className="form-group focused">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-first-name"
+                                >
+                                  First name
+                                </label>
+                                <input
+                                  type="text"
+                                  id="input-first-name"
+                                  className="form-control form-control-alternative"
+                                  placeholder="First name"
+                                  defaultValue=""
+                                />
+                              </div>
+                            </div>
+                            <div className="col-lg-6">
+                              <div className="form-group focused">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-last-name"
+                                >
+                                  Last name
+                                </label>
+                                <input
+                                  type="text"
+                                  id="input-last-name"
+                                  className="form-control form-control-alternative"
+                                  placeholder="Last name"
+                                  defaultValue=""
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <hr className="my-4" />
+                        {/* Address */}
+                        <h6 className="heading-small text-muted mb-4">
+                          Contact information
+                        </h6>
+                        <div className="pl-lg-4">
+                          <div className="row">
+                            <div className="col-md-12">
+                              <div className="form-group focused">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-address"
+                                >
+                                  Address
+                                </label>
+                                <input
+                                  id="input-address"
+                                  className="form-control form-control-alternative"
+                                  placeholder="Home Address"
+                                  defaultValue=""
+                                  type="text"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-lg-4">
+                              <div className="form-group focused">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-city"
+                                >
+                                  City
+                                </label>
+                                <input
+                                  type="text"
+                                  id="input-city"
+                                  className="form-control form-control-alternative"
+                                  placeholder="City"
+                                />
+                              </div>
+                            </div>
+                            <div className="col-lg-4">
+                              <div className="form-group focused">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-country"
+                                >
+                                  Country
+                                </label>
+                                <input
+                                  type="text"
+                                  id="input-country"
+                                  className="form-control form-control-alternative"
+                                  placeholder="Country"
+                                />
+                              </div>
+                            </div>
+                            <div className="col-lg-4">
+                              <div className="form-group">
+                                <label
+                                  className="form-control-label"
+                                  htmlFor="input-country"
+                                >
+                                  Postal code
+                                </label>
+                                <input
+                                  type="number"
+                                  id="input-postal-code"
+                                  className="form-control form-control-alternative"
+                                  placeholder="Postal code"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <hr className="my-4" />
+                        {/* Description */}
+                        <h6 className="heading-small text-muted mb-4">About me</h6>
+                        <div className="pl-lg-4">
+                          <div className="form-group focused">
+                            <label>About Me</label>
+                            <textarea
+                              rows={4}
+                              className="form-control form-control-alternative"
+                              placeholder="A few words about you ..."
+                            />
+                          </div>
+                        </div>
+                      </form>
+                    </div>
                   </div>
-                </div>
-                <div className="card-body">
-                  <form>
-                    <h6 className="heading-small text-muted mb-4">
-                      User information
-                    </h6>
-                    <div className="pl-lg-4">
-                      <div className="row">
-                        <div className="col-lg-6">
-                          <div className="form-group focused">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-username"
-                            >
-                              Username
-                            </label>
-                            <input
-                              type="text"
-                              id="input-username"
-                              className="form-control form-control-alternative"
-                              placeholder="Username"
-                              defaultValue="lucky.jesse"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-6">
-                          <div className="form-group">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-email"
-                            >
-                              Email address
-                            </label>
-                            <input
-                              type="email"
-                              id="input-email"
-                              className="form-control form-control-alternative"
-                              placeholder="jesse@example.com"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-lg-6">
-                          <div className="form-group focused">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-first-name"
-                            >
-                              First name
-                            </label>
-                            <input
-                              type="text"
-                              id="input-first-name"
-                              className="form-control form-control-alternative"
-                              placeholder="First name"
-                              defaultValue="Lucky"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-6">
-                          <div className="form-group focused">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-last-name"
-                            >
-                              Last name
-                            </label>
-                            <input
-                              type="text"
-                              id="input-last-name"
-                              className="form-control form-control-alternative"
-                              placeholder="Last name"
-                              defaultValue="Jesse"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <hr className="my-4" />
-                    {/* Address */}
-                    <h6 className="heading-small text-muted mb-4">
-                      Contact information
-                    </h6>
-                    <div className="pl-lg-4">
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="form-group focused">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-address"
-                            >
-                              Address
-                            </label>
-                            <input
-                              id="input-address"
-                              className="form-control form-control-alternative"
-                              placeholder="Home Address"
-                              defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                              type="text"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-lg-4">
-                          <div className="form-group focused">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-city"
-                            >
-                              City
-                            </label>
-                            <input
-                              type="text"
-                              id="input-city"
-                              className="form-control form-control-alternative"
-                              placeholder="City"
-                              defaultValue="New York"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-4">
-                          <div className="form-group focused">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-country"
-                            >
-                              Country
-                            </label>
-                            <input
-                              type="text"
-                              id="input-country"
-                              className="form-control form-control-alternative"
-                              placeholder="Country"
-                              defaultValue="United States"
-                            />
-                          </div>
-                        </div>
-                        <div className="col-lg-4">
-                          <div className="form-group">
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-country"
-                            >
-                              Postal code
-                            </label>
-                            <input
-                              type="number"
-                              id="input-postal-code"
-                              className="form-control form-control-alternative"
-                              placeholder="Postal code"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <hr className="my-4" />
-                    {/* Description */}
-                    <h6 className="heading-small text-muted mb-4">About me</h6>
-                    <div className="pl-lg-4">
-                      <div className="form-group focused">
-                        <label>About Me</label>
-                        <textarea
-                          rows={4}
-                          className="form-control form-control-alternative"
-                          placeholder="A few words about you ..."
-                          defaultValue={
-                            "Write something interesting about you:)."
-                          }
-                        />
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
+                  <input className="btn btn-info align-middle ms-3 mt-3 mb-3 text-color: white" type="submit" value="Save Profile"/>
+                </form>
             </div>
+
           </div>
         </div>
       </div>
@@ -417,7 +419,7 @@ export default function EditProfile() {
 //               </label>
 //               <br></br>
 //               <br></br>
-//               <input type="submit" value="Submit" />
+//               <input type="submit" value="Save" />
 //             </form>
 //           </div>
 //         </div>
